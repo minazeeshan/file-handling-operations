fe257b9e9c8ca31844426081b2b84fe2ab568b44
minazeeshan <94472730+minazeeshan@users.noreply.github.com>
2023-05-27 23:29:40 +0500

    Add files via upload

---
+++ Data read from file and entered into array/Data read from file and entered into array/Module1.vb
@@ -0,0 +1,29 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim fileLine(0 To 2) As String
+        Dim hashPos As Integer
+        Dim index As Integer
+        Dim NamesArray(0 To 2) As String
+        Dim IDArray(0 To 2) As String
+        hashPos = 0
+        index = 0
+        FileOpen(1, "ArrayToFile.txt", OpenMode.Input)
+        While Not EOF(1) Or index <= 2
+            fileLine(index) = LineInput(1)
+            index = index + 1
+        End While
+        For i = 0 To 2
+            hashPos = InStr(fileLine(i), "*")
+            NamesArray(i) = Left(fileLine(i), hashPos - 1)
+            IDArray(i) = Right(fileLine(i), Len(fileLine(i)) - hashPos)
+        Next
+        FileClose(1)
+        For c = 0 To 2
+            Console.WriteLine(NamesArray(c))
+            Console.WriteLine(IDArray(c))
+        Next
+        Console.ReadKey()
+    End Sub
+
+End Module
---
+++ writing data from array to file/Array to file.vb
@@ -0,0 +1,26 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim NamesArray(0 To 2) As String
+        Dim IDArray(0 To 2) As String
+        Dim fileLine As String
+        FileOpen(1, "ArrayToFile.txt", OpenMode.Output)
+        For i = 1 To 3
+            Console.WriteLine("Input name no. " & i & ":")
+            NamesArray(i - 1) = Console.ReadLine
+            Console.WriteLine("Input ID no. " & i & ":")
+            IDArray(i - 1) = Console.ReadLine
+        Next
+        'combining name and ID into a single string and then storing it in the file
+
+        For c = 0 To 2
+            fileLine = NamesArray(c) & "*" & IDArray(c)
+            PrintLine(1, fileLine)
+        Next
+        FileClose(1)
+        Console.ReadKey()
+
+
+    End Sub
+
+End Module
